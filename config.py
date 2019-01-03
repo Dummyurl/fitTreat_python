a5d9af1967f923019d64a164f03918f80c5af3e0
Balkrishna Meena <balu251994@gmail.com>
2019-01-04 00:10:58 +0530

    test commit - local setup

--- config.py
+++ config.py
@@ -12,16 +12,17 @@ class Config(object):
     crptrKey = b'WWUV2cX5GVM5K2iFu_MauyOoecTvUNGabtpG4z8TAEY='
     port = os.getenv("PORT") or 8888
     s3URL = 'https://s3.us-east-2.amazonaws.com/fittreatstorage/meal_images_dev/'
-    userId = 'app116066240@heroku.com'
+    userId = 'app116066240@heroku.com' # fitTreat Heroku Account
     password = 'Welcome12#'
     dbName = 'fitdb'
-    uri = env.uris[0] or 'localhost:8888'
+    # uri = env.uris[0] or 'localhost:8888'
+    uri = 'localhost:8888'
     MONGODB_SETTINGS = {
         'db': dbName,
-        'host': 'mongodb://conusr:Welcome1@ds113454.mlab.com/' + dbName,
-        'port': 13454,
-        #'host': '127.0.0.1',
-        #'port': 27017,
-        'username': 'conusr',
-        'password': 'Welcome1'
+        #'host': 'mongodb://conusr:Welcome1@ds113454.mlab.com/' + dbName,
+        #'port': 13454,
+        'host': '127.0.0.1',
+        'port': 27017,
+        #'username': 'conusr',
+        #'password': 'Welcome1'
     }
