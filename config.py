5249b5145206ea84ba990798dd5fabe8abf34464
Balkrishna Meena <balu251994@gmail.com>
2019-01-05 03:31:24 +0530

    config variables

--- config.py
+++ config.py
@@ -12,15 +12,14 @@ class Config(object):
     crptrKey = b'WWUV2cX5GVM5K2iFu_MauyOoecTvUNGabtpG4z8TAEY='
     port = os.getenv("PORT") or 8888
     s3URL = 'https://s3.us-east-2.amazonaws.com/fittreatstorage/meal_images_dev/'
-    userId = 'app120884642@heroku.com' # fitTreat Heroku Account
-    smtp_host = 'smtp.sendgrid.net'
-    smtp_port = 465
-    password = '7ipagxql9861'
+    userId = os.getenv("SENDGRID_USERNAME")
+    print("SMTP User Name : " + userId)
+    password = os.getenv("SENDGRID_PASSWORD")
+    smtp_host = os.getenv("SMTP_HOST")
+    print("SMTP Host : " + smtp_host)
+    smtp_port = os.getenv("SMTP_PORT")
     dbName = 'fit_treat'
-    # uri = env.uris[0] or 'localhost:8888'
-    # uri = 'localhost:8888'
     MONGODB_SETTINGS = {
         'db': dbName,
         'host': 'mongodb://fitTreat:Welcome12#@ds119343.mlab.com:19343/' + dbName,
-        # 'port': 27017,
     }
